45719daa7b4b05ffbfa8fa197d7f5431783f4148
PeiSheng <easonlin0925250286@gmail.com>
2025-11-25 03:26:40 +0800

    string --key重複乘以ASCII
    cat     8
    dog     6
    bat     0
    cow     1
    ant     4
    owl     0
    bee     8
    hen     6
    pig     0
    fox     8

--- Assignment/AssignmentIV/CXX/hash_fn.cpp
+++ Assignment/AssignmentIV/CXX/hash_fn.cpp
@@ -22,10 +22,10 @@ int myHashInt(int key, int m) {
 }
 
 int myHashString(const std::string& str, int m) {
-    unsigned long hash = 0;
+    unsigned long hash = 1;
     for (char c : str) {
         int val = (int)c;   // 將字元轉成對應的 ASCII 數字
-        hash += val * 2654435761;
+        hash *= val * 2654435761;
     }
     return static_cast<int>(hash % m);  // basic division method
 }
